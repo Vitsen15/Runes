e5b1315b0e7260980a739c2e632826b79ca724f9
Vitsentovich <vitsentovich.utd@gmail.com>
2017-10-29 10:42:27 +0200

    add sockets controller

---
+++ application/controller/Sockets.php
@@ -0,0 +1,18 @@
+<?php
+
+class Sockets extends Controller {
+    const MAX_SOCKETS = 6;
+    public $sockets;
+
+    function __construct($model = null) {
+        parent::__construct($model);
+
+        $this->fillSockets();
+    }
+
+    private function fillSockets() {
+        for ($i = 0; $i <= 6; $i++) {
+            $this->sockets[$i] = [$i];
+        }
+    }
+}
