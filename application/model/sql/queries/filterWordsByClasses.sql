7cb19aa0a00593ddb8250fa3229ff51a2fcd45f2
Vitsentovich <vitsentovich.utd@gmail.com>
2017-11-30 22:22:47 +0200

    change classes model filter for new db structure

--- application/model/sql/queries/filterWordsByClasses.sql
+++ application/model/sql/queries/filterWordsByClasses.sql
@@ -1,9 +1,8 @@
 SELECT group_concat(words.id) AS words
-FROM (SELECT words.id
-      FROM words
-        INNER JOIN words_word_properties ON words_word_properties.runes_word_id = words.id
-        INNER JOIN word_properties ON word_properties.id = words_word_properties.runes_word_property_id
-        INNER JOIN classes_word_properties ON classes_word_properties.runes_word_property_id = words_word_properties.id
-        INNER JOIN classes ON classes.id = classes_word_properties.class_id
+FROM (SELECT word_properties.id
+      FROM word_properties
+        JOIN property_type ON property_type.id = word_properties.property_type_id
+        JOIN classes_property_type ON classes_property_type.property_type_id = property_type.id
+        JOIN classes ON classes.id = classes_property_type.class_id
       WHERE find_in_set(cast(classes.id AS CHAR), :classes)
-      GROUP BY words.id) AS words
+      GROUP BY word_properties.id) AS words
